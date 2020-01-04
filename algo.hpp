c9456ee38b0cb44a72b9895e5c3ad857bfded777
vegeka <zhj199305@gmail.com>
2020-01-03 20:51:44 -0500

    define algo and cube

---
+++ algo.hpp
@@ -0,0 +1,20 @@
+#ifndef ALGO_H
+#define ALGO_H
+
+#include "cube.hpp"
+#include <vector>
+#include <memory>
+
+using namespace std;
+
+namespace cube{
+typedef pair<face_type, int> move_step_t;
+typedef vector<move_step_t> move_seq_t;
+
+class algo{
+            
+};
+
+}
+
+#endif
--- cube.hpp
+++ cube.hpp
@@ -14,24 +14,26 @@ struct face{
     enum face_type{
         top = 0, bottom = 1, front = 2, back = 3, left = 4, right = 5
     };
-    int c[9];
+    int8_t c[9];
 };
 
 struct block{
-    int top, bottom, front, back, left, right;
+    int8_t top, bottom, front, back, left, right;
 };
 
-typedef pair<const int*, const int*> block_info_t;
+typedef pair<const int8_t*, const int8_t*> block_info_t;
 
 class cube{
     public:
         cube();
-
         block get_block(int*, int, int) const;
-        block_info get_corner() const;
-        block_info get_edge() const;
-
+        block_info_t get_corner() const;
+        block_info_t get_edge() const;
         void rotate(face_type, int count = 1);
+
+    private:
+        int8_t cp[8], co[8]; // cornor's position and orientation
+        int8_t ep[12], eo[12]; // edge's position and orientation
 };
 }
 #endif
